9b50f0ab7820a85af004611984abc8d745352021
Mamadou Sissoko <msissoko2317@gmail.com>
2025-11-02 18:51:13 -0800

    Milestone 3 actual

--- 210-lab-27.cpp
+++ 210-lab-27.cpp
@@ -26,7 +26,7 @@ int main() {
     villagerData["Raymond"] = make_tuple(8, "Cat", "Hello there!");
     villagerData.insert({"Marshal", make_tuple(7, "Squirrel", "Hey hey hey!")});
 
-   int choice
+   int choice;
    string name;
 
    do {
@@ -39,18 +39,53 @@ int main() {
         cin >> choice; 
 
         switch(choice) {
-            case 1:
+            case 1: // Increasing friendship
                 cout << "Enter villager name: ";
                 cin >> name;
+                 if (villagerData.find(name) != villagerData.end()) {
+                    get<0>(villagerData[name]) += 1;
+                    cout << name << "'s friendship increased.\n";
+                } else {
+                    cout << name << " not found.\n";
+                }
+                printVillagers(villagerData);
+                break;
+            case 2: // Decreasing friendship
+                 cout << "Enter villager name: ";
+                cin >> name;
+                if (villagerData.find(name) != villagerData.end()) {
+                    if (get<0>(villagerData[name]) > 0)
+                        get<0>(villagerData[name]) -= 1;
+                    cout << name << "'s friendship decreased.\n";
+                } else {
+                    cout << name << " not found.\n";
+                }
+                printVillagers(villagerData);
+                break;
+            case 3: // Searches for villager
+                 cout << "Enter villager name: ";
+                cin >> name;
+                if (villagerData.find(name) != villagerData.end()) {
+                    auto& v = villagerData[name];
+                    cout << name << " [" 
+                         << get<0>(v) << ", "
+                         << get<1>(v) << ", "
+                         << get<2>(v) << "]\n";
+                } else {
+                    cout << name << " not found.\n";
+                }
+                break;
+            case 4: // Exits program
+                cout << "Exiting program.\n";
+                break;
 
-            case 2:
-
-            case 3:
-
-            case 4:
-   
+            default:
+                cout << "Invalid choice. Try again.\n";
         }
-         } return 0;
+
+    } while(choice != 4);
+        
+          return 0;
 
         }
    
